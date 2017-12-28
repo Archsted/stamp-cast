9da7b0edc0af0e6131e40394c99163e598977ab6
Archsted <archsted@gmail.com>
2017-12-29 04:03:27 +0900

    スタンプの基本機能をVue.jsに移行

--- resources/assets/js/broadcaster.js
+++ resources/assets/js/broadcaster.js
@@ -1,91 +1,20 @@
 require('./bootstrap');
+require('./echo');
+
+import stampDisplay from './components/stamp_display';
 
 const app = new Vue({
     el: '#app',
     data: {
-        counter: 0,
+
+    },
+    components: {
+        'stamp-display': stampDisplay
     },
     created: function () {
         echo.channel('general')
             .listen('StampEvent', (e) => {
-                // console.log(e);
-                this.add();
+                app.$refs.stamp.testAdd();
             });
     },
-    methods: {
-        add: function () {
-            let stamp = {
-                id: 1,
-                path: 'bg1.png'
-            };
-
-            this.addStamp(stamp);
-        },
-        addStamp: function(stamp) {
-            let img = new Image();
-
-            img.onload = () => {
-                // クラス、スタイルの定義
-                img.classList.add('stamp');
-                img.style.top = this.getRandomTop(img.height) + 'px';
-                img.style.left = this.getRandomLeft(img.width) + 'px';
-
-                // 要素の追加
-                document.body.appendChild(img);
-
-                // カウンターを増やす
-                this.counter++;
-
-                let basicTimeLine = animejs.timeline({
-                    complete: () => {
-                        // タイムラインが全て終わったら自分自身を削除し、カウンターを減らす
-                        document.body.removeChild(img);
-                        this.counter--;
-                    }
-                });
-
-                // スタンプのライムライン定義
-                basicTimeLine
-                    .add({
-                        targets: img,
-                        scale: {
-                            value: [0.2, 1],
-                        },
-                        duration: 200,
-                        opacity: 1.0,
-                        easing: 'easeInOutSine'
-                    })
-                    .add({
-                        targets: img,
-                        scale: 0.5,
-                        duration: 500,
-                        opacity: 0,
-                        delay: 3000,
-                        easing: 'easeInOutSine'
-                    });
-
-            };
-
-            img.src = stamp.path;
-        },
-
-        getRandomTop: function (offset) {
-            return Math.floor(Math.random() * (document.documentElement.clientHeight - offset));
-        },
-
-        getRandomLeft: function (offset) {
-            return Math.floor(Math.random() * (document.body.clientWidth - offset));
-        },
-
-        sendStamp: function () {
-            axios.get('/doStamp')
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-        },
-    },
 });
-
--- resources/assets/js/listener.js
+++ resources/assets/js/listener.js
@@ -1,4 +1,5 @@
 require('./bootstrap');
+require('./echo');
 
 echo.channel('general')
     .listen('StampEvent', (e) => {
