03b72a30d585a74cbb534bbac20ca59c42f8ad64
Archsted <archsted@gmail.com>
2018-05-12 21:16:12 +0900

    スタンプ帳のマイグレーションファイルを追加

--- app/Favorites.php
+++ app/Favorites.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorites extends Model
 {
-    //
+    public function stamp()
+    {
+        return $this->belongsTo('App\Stamp');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
